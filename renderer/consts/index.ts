99bb5decd91170c66f137a7d4e75daee1ba59622
ewc340 <edward.choi@panchaininc.com>
2020-08-12 23:35:53 -0700

    Lint consts

--- renderer/consts/index.ts
+++ renderer/consts/index.ts
@@ -1,3 +1,12 @@
-export { AlertActionsTypes, ConsoleActionsTypes, PeripheralActionsTypes, EditorActionsTypes, GamepadsActionsTypes, SettingsActionsTypes, InfoActionsTypes, FieldActionsTypes} from './actions';
+export {
+  AlertActionsTypes,
+  ConsoleActionsTypes,
+  PeripheralActionsTypes,
+  EditorActionsTypes,
+  GamepadsActionsTypes,
+  SettingsActionsTypes,
+  InfoActionsTypes,
+  FieldActionsTypes,
+} from './actions';
 export { PeripheralTypes } from './peripheral-types';
 export { VERSION } from './version';
