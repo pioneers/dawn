4daaa633c24a9d587bdb0e5bbbf5a7b5afaf138e
ewc340 <edward.choi@panchaininc.com>
2020-08-12 22:32:22 -0700

    Lint actions

--- renderer/actions/EditorActions.ts
+++ renderer/actions/EditorActions.ts
@@ -2,48 +2,48 @@ import * as consts from '../consts';
 import { EditorActions } from '../types';
 
 export const editorUpdate: EditorActions['editorUpdate'] = (newVal: string) => ({
-    type: consts.EditorActionsTypes.UPDATE_EDITOR,
-    code: newVal
-})
+  type: consts.EditorActionsTypes.UPDATE_EDITOR,
+  code: newVal,
+});
 
 export const openFileSucceeded: EditorActions['openFileSucceeded'] = (data: string, filepath: string) => ({
-    type: consts.EditorActionsTypes.OPEN_FILE_SUCCEEDED,
-    code: data,
-    filepath
-})
+  type: consts.EditorActionsTypes.OPEN_FILE_SUCCEEDED,
+  code: data,
+  filepath,
+});
 
 export const saveFileSucceeded: EditorActions['saveFileSucceeded'] = (data: string, filepath: string) => ({
-    type: consts.EditorActionsTypes.SAVE_FILE_SUCCEEDED,
-    code: data,
-    filepath,
-})
+  type: consts.EditorActionsTypes.SAVE_FILE_SUCCEEDED,
+  code: data,
+  filepath,
+});
 
 export const openFile: EditorActions['openFile'] = () => ({
-    type: consts.EditorActionsTypes.OPEN_FILE,
+  type: consts.EditorActionsTypes.OPEN_FILE,
 });
 
 export const dragFile: EditorActions['dragFile'] = (filepath: string) => ({
-    type: consts.EditorActionsTypes.DRAG_FILE,
-    filepath,
+  type: consts.EditorActionsTypes.DRAG_FILE,
+  filepath,
 });
 
-export const saveFile: EditorActions['saveFile'] = (saveAs: boolean = false) => ({
-    type: consts.EditorActionsTypes.SAVE_FILE,
-    saveAs,
+export const saveFile: EditorActions['saveFile'] = (saveAs = false) => ({
+  type: consts.EditorActionsTypes.SAVE_FILE,
+  saveAs,
 });
 
 export const deleteFile: EditorActions['deleteFile'] = () => ({
-    type: consts.EditorActionsTypes.DELETE_FILE,
+  type: consts.EditorActionsTypes.DELETE_FILE,
 });
 
 export const createNewFile: EditorActions['createNewFile'] = () => ({
-    type: consts.EditorActionsTypes.CREATE_NEW_FILE,
+  type: consts.EditorActionsTypes.CREATE_NEW_FILE,
 });
 
 export const downloadCode: EditorActions['downloadCode'] = () => ({
-    type: consts.EditorActionsTypes.DOWNLOAD_CODE,
+  type: consts.EditorActionsTypes.DOWNLOAD_CODE,
 });
 
 export const uploadCode: EditorActions['uploadCode'] = () => ({
-    type: consts.EditorActionsTypes.UPLOAD_CODE,
+  type: consts.EditorActionsTypes.UPLOAD_CODE,
 });
