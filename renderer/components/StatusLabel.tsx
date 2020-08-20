b6a5f6cecb15de28e8833a3b6679739c29f2d36c
Hailey Jang <hailey_jang@berkeley.edu>
2020-08-20 14:30:08 -0700

    Edited StatusLabel.tsx

--- renderer/components/StatusLabel.tsx
+++ renderer/components/StatusLabel.tsx
@@ -1,23 +1,38 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Label } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import numeral from 'numeral';
 
-const StatusLabelComponent = (props) => {
+interface StateProps {
+  connectionStatus: boolean;
+  runtimeStatus: boolean;
+  batteryLevel: number;
+  batterySafety: boolean;
+  blueMasterTeamNumber: number;
+  goldMasterTeamNumber: number;
+  ipAddress: string;
+}
+
+interface OwnProps {
+  fieldControlStatus: boolean;
+}
+
+type Props = StateProps & OwnProps;
+
+const StatusLabelComponent = (props: Props) => {
   let labelStyle = 'default';
   let labelText = 'Disconnected';
   const masterRobotHeader = 'Master Robot: Team ';
   const teamIP = props.ipAddress.substring(props.ipAddress.length - 2, props.ipAddress.length);
-  const shouldDisplayMaster = teamNumber => parseInt(teamIP, 10) === teamNumber
+  const shouldDisplayMaster = (teamNumber: number) => parseInt(teamIP, 10) === teamNumber
                                             && props.fieldControlStatus;
   let masterRobot = null;
   let masterRobotStyle = ' ';
-  if (shouldDisplayMaster(props.blueMaster)) {
-    masterRobot = props.blueMaster;
+  if (shouldDisplayMaster(props.blueMasterTeamNumber)) {
+    masterRobot = props.blueMasterTeamNumber;
     masterRobotStyle = 'primary';
-  } else if (shouldDisplayMaster(props.goldMaster)) {
-    masterRobot = props.goldMaster;
+  } else if (shouldDisplayMaster(props.goldMasterTeamNumber)) {
+    masterRobot = props.goldMasterTeamNumber;
     masterRobotStyle = 'warning';
   }
 
@@ -44,28 +59,14 @@ const StatusLabelComponent = (props) => {
   );
 };
 
-StatusLabelComponent.propTypes = {
-  connectionStatus: PropTypes.bool.isRequired,
-  runtimeStatus: PropTypes.bool.isRequired,
-  batteryLevel: PropTypes.number.isRequired,
-  batterySafety: PropTypes.bool.isRequired,
-  blueMaster: PropTypes.number.isRequired,
-  goldMaster: PropTypes.number.isRequired,
-  ipAddress: PropTypes.string.isRequired,
-  fieldControlStatus: PropTypes.bool.isRequired,
-};
-
-
-const mapStateToProps = state => ({
+const mapStateToProps = (state: ApplicationState) => ({
   batteryLevel: state.peripherals.batteryLevel,
   batterySafety: state.peripherals.batterySafety,
   masterStatus: state.fieldStore.masterStatus,
-  blueMaster: state.fieldStore.blueMaster,
-  goldMaster: state.fieldStore.goldMaster,
+  blueMasterTeamNumber: state.fieldStore.blueMasterTeamNumber,
+  goldMasterTeamNumber: state.fieldStore.goldMasterTeamNumber,
   ipAddress: state.info.ipAddress,
   fieldControlStatus: state.fieldStore.fieldControl,
 });
 
-const StatusLabel = connect(mapStateToProps)(StatusLabelComponent);
-
-export default StatusLabel;
+export const StatusLabel = connect(mapStateToProps)(StatusLabelComponent);
