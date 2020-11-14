796e4c66eadb509d9b71094ab126a01a06aefaa5
ewc340 <edwardc1028@berkeley.edu>
2020-11-14 15:19:01 -0800

    Add comment about Peripheral type

--- renderer/types/peripheral-list.ts
+++ renderer/types/peripheral-list.ts
@@ -1,5 +1,8 @@
 import { Param } from "../../protos/protos";
 
+/** This `Peripheral` type is separate from the Device proto. 
+ *  The main difference is the uid is a string to account for readability of numbers and 64-bit Longs.
+ */
 export interface Peripheral {
   /** Device name */
   name: string;
