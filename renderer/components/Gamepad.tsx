64133fd298e04bd363bc39b162bcacffd13f6be8
meshankhosla <meshankhosla@gmail.com>
2020-11-18 20:37:07 -0800

    Converted Gamepad to functional component

--- renderer/components/Gamepad.tsx
+++ renderer/components/Gamepad.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Modal,
   Button,
@@ -16,99 +16,87 @@ interface OwnProps {
 
 type Props = OwnProps;
 
-interface State {
-  showModal: boolean;
-}
-
 const NUM_GAMEPAD_BUTTONS = 17;
 const NUM_GAMEPAD_AXES = 4;
 
-export class Gamepad extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = { showModal: false };
-  }
+/**
+ * Each individual gamepad that is connected
+ */
+export const Gamepad = (props: Props) => {
+    const [showModal, setModal] = useState(false);
 
-  closeModal = () => {
-    this.setState({ showModal: false });
-  }
+    const roundedValues = () => {
+        const gamepadButtons: string[] = [];
+      
+        for (let i = 0; i < 32; i++) {
+          gamepadButtons.push(numeral(props.gamepad.buttons | (1 << i)).format('0'));
+        }
+    
+        return {
+          axes: _.map(props.gamepad.axes, (axis: number) => numeral(axis).format('0.00000')),
+          buttons: gamepadButtons
+        };
+    }
 
-  openModal = () => {
-    this.setState({ showModal: true });
-  }
-
-  roundedValues = () => {
-    const gamepadButtons: string[] = [];
+    const renderHeader = () => {
+        return (
+          <div>
+            <h4 style={{ display: 'inline' }}> Gamepad {props.index} </h4>
+          </div>
+        );
+      }
+      const { gamepad } = props;
   
-    for (let i = 0; i < 32; i++) {
-      gamepadButtons.push(numeral(this.props.gamepad.buttons | (1 << i)).format('0'));
-    }
-
-    return {
-      axes: _.map(this.props.gamepad.axes, (axis: number) => numeral(axis).format('0.00000')),
-      buttons: gamepadButtons
-    };
-  }
-
-  renderHeader = () => {
-    return (
-      <div>
-        <h4 style={{ display: 'inline' }}> Gamepad {this.props.index} </h4>
-      </div>
-    );
-  }
-
-  render() {
-
-    const { gamepad } = this.props;
-    const { showModal } = this.state;
-
     if (!gamepad) {
-      return (<div />);
+        return (<div />);
     }
-    const values = this.roundedValues();
-    return (
-      <ListGroupItem>
-        <div style={{ overflow: 'auto', width: '100%' }}>
-          <span style={{ float: 'left' }}>{this.renderHeader()}</span>
-          <Button type="button" bsSize="small" onClick={this.openModal} style={{ float: 'right' }}>
-            Details
-          </Button>
-        </div>
-        <Modal show={showModal} onHide={this.closeModal}>
-          <Modal.Header closeButton>
-            <Modal.Title>{this.renderHeader()}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <img alt="gamepad" src="graphics/gamepad.png" style={{ width: '100%' }} />
-            <Table bordered>
-              <tbody>
-                <tr>
-                  <th>Button</th>
-                  {_.range(NUM_GAMEPAD_BUTTONS).map((gamepadButtonNumber: number) => <td>{gamepadButtonNumber}</td>)}
-                </tr>
-                <tr>
-                  <th>Value</th>
-                  {_.range(NUM_GAMEPAD_BUTTONS).map((gamepadButtonNumber: number) => <td>{values.buttons[gamepadButtonNumber]}</td>)}
-                </tr>
-              </tbody>
-            </Table>
-            <Table bordered>
-              <tbody>
-                <tr>
-                  <th>Axis</th>
-                  {_.range(NUM_GAMEPAD_AXES).map((gamepadButtonAxis: number) => <td>{gamepadButtonAxis}</td>)}
 
-                </tr>
-                <tr>
-                  <th>Value</th>
-                  {_.range(NUM_GAMEPAD_AXES).map((gamepadButtonAxis: number) => <td>{values.buttons[gamepadButtonAxis]}</td>)}
-                </tr>
-              </tbody>
-            </Table>
-          </Modal.Body>
-        </Modal>
-      </ListGroupItem>
+    const values = roundedValues();
+
+    return (
+        <ListGroupItem>
+          <div style={{ overflow: 'auto', width: '100%' }}>
+            <span style={{ float: 'left' }}>{renderHeader()}</span>
+            <Button type="button" bsSize="small" onClick={ () => setModal(true) } style={{ float: 'right' }}>
+              Details
+            </Button>
+          </div>
+          <Modal show={ showModal } onHide={ () => setModal(false) }>
+            <Modal.Header closeButton>
+              <Modal.Title>{renderHeader()}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <img alt="gamepad" src="graphics/gamepad.png" style={{ width: '100%' }} />
+              <Table bordered>
+                <tbody>
+                  <tr>
+                    <th>Button</th>
+                    {_.range(NUM_GAMEPAD_BUTTONS).map((gamepadButtonNumber: number) => <td>{gamepadButtonNumber}</td>)}
+                  </tr>
+                  <tr>
+                    <th>Value</th>
+                    {_.range(NUM_GAMEPAD_BUTTONS).map((gamepadButtonNumber: number) => <td>{values.buttons[gamepadButtonNumber]}</td>)}
+                  </tr>
+                </tbody>
+              </Table>
+              <Table bordered>
+                <tbody>
+                  <tr>
+                    <th>Axis</th>
+                    {_.range(NUM_GAMEPAD_AXES).map((gamepadButtonAxis: number) => <td>{gamepadButtonAxis}</td>)}
+  
+                  </tr>
+                  <tr>
+                    <th>Value</th>
+                    {_.range(NUM_GAMEPAD_AXES).map((gamepadButtonAxis: number) => <td>{values.buttons[gamepadButtonAxis]}</td>)}
+                  </tr>
+                </tbody>
+              </Table>
+            </Modal.Body>
+          </Modal>
+        </ListGroupItem>
     );
-  }
-};
+}
+
+    
+    
