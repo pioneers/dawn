165c59a826f7b1e55355a776f359dc9c4bda8302
Lance Jackson <lanceisajackson@berkeley.edu>
2020-11-21 14:33:30 -0800

    refactor ConsoleOutput with useEffect

--- renderer/components/ConsoleOutput.tsx
+++ renderer/components/ConsoleOutput.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Panel } from 'react-bootstrap';
 
 interface StateProps {
@@ -14,75 +14,64 @@ interface OwnProps {
 
 type Props = StateProps & OwnProps;
 
-export class ConsoleOutput extends React.Component<Props> {
-  outerDiv: HTMLDivElement | null;
-  constructor(props: Props) {
-    super(props);
-  }
+export function ConsoleOutput(props: Props) {
+  let outerDiv: HTMLDivElement | null;
+  let { show, output } = props;
 
-  componentDidMount = () => {
-    this.scrollToBottom();
-  }
-
-  componentWillReceiveProps = (nextProps: Props) => {
-    if (this.props.output.length === 0 && nextProps.output.length > 0 && !this.props.show) {
-      this.props.toggleConsole();
+  const prevOutputRef = useRef([] as string[]);
+  const prevOutput = prevOutputRef.current;
+  useEffect(() => {
+    prevOutputRef.current = output;
+    scrollToBottom();
+    if (prevOutput.length === 0 && output.length > 0 && !show) {
+      props.toggleConsole();
     }
-  }
-
-  componentDidUpdate = () => {
-    this.scrollToBottom();
-  }
-
-  scrollToBottom = () => {
-    if (!this.props.disableScroll) {
-      if (this.outerDiv !== null) {
-        this.outerDiv.scrollTop = this.outerDiv.scrollHeight;
+  });
+  
+  function scrollToBottom() {
+    if (!props.disableScroll) {
+      if (outerDiv !== null) {
+        outerDiv.scrollTop = outerDiv.scrollHeight;
       }
     }
   }
 
-
-  render() {
-    const { show, output } = this.props;
-
-    const height = `${String(this.props.height)}px`; // TODO: Use Panel.Collapse
-    return (
-      <div>
-        <Panel
-          style={{
-            display: show ? 'block' : 'none',
-            marginBottom: '0',
-            borderRadius: '0',
-          }}
-        >
-          <Panel.Body>
-            <pre
+  const height = `${String(props.height)}px`; // TODO: Use Panel.Collapse
+  return (
+    <div>
+      <Panel
+        style={{
+          display: show ? 'block' : 'none',
+          marginBottom: '0',
+          borderRadius: '0',
+        }}
+      >
+        <Panel.Body>
+          <pre
+            style={{
+              position: 'relative',
+              margin: '0',
+              height,
+            }}
+          >
+            <div
               style={{
-                position: 'relative',
-                margin: '0',
-                height,
+                position: 'absolute',
+                bottom: '0',
+                maxHeight: height,
+                overflowY: 'auto',
+                padding: '20px',
+                width: '99%',
               }}
+              ref={(el) => { outerDiv = el; }}
             >
-              <div
-                style={{
-                  position: 'absolute',
-                  bottom: '0',
-                  maxHeight: height,
-                  overflowY: 'auto',
-                  padding: '20px',
-                  width: '99%',
-                }}
-                ref={(el) => { this.outerDiv = el; }}
-              >
-                {output.map(line => (
-                  <code key={`${line}-Code-${Math.random()}`}>{line}</code>
-                ))}
-              </div>
-            </pre>
-          </Panel.Body>
-        </Panel>
-      </div>
-    );
-  }
+              {output.map((line: string) => (
+                <code key={`${line}-Code-${Math.random()}`}>{line}</code>
+              ))}
+            </div>
+          </pre>
+        </Panel.Body>
+      </Panel>
+    </div>
+  );
 }
