17e4f9130535e985d349f19719ffa52b29febf72
Hailey Jang <hailey_jang@berkeley.edu>
2020-12-24 13:18:08 +0900

    Updated the react portion

--- renderer/components/CheckCodeChallengesModal.tsx
+++ renderer/components/CheckCodeChallengesModal.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Modal, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Modal, Row, Col, FormControl, Form, FormGroup } from "react-bootstrap";
 
 interface OwnProps {
     shouldShow: boolean;
@@ -8,42 +8,40 @@ interface OwnProps {
   
 type Props = OwnProps;
   
-interface State {
-    inputs: Object;
-}
 
-class CheckCodeChallengesModal extends React.Component<Props,State>{
-    constructor(props: Props){
-        super(props);
+const CheckCodeChallengesModal = (props: Props) => {
 
-        this.state= {
-            inputs: {}, //this will be the objects that keeps the function names
-        }
-        
-    }
+    const [inputs, changeInputs] = useState({});
+    const [outputs, changeOutputs] = useState({}); //to be done later. 
 
-    loadFunctions() {
+
+    const loadFunctions = () => {
         //this is where we will parse the code. Regex time maaaaaaannnn
     }
 
-    render() {
-        
-        this.loadFunctions();
+    const handleInputChange = (e: React.FormEvent<FormControl & HTMLInputElement>) => {
+        //changeInputs({ *FunctionNameHere* : parseInt(e.currentTarget.value) });
+    }
 
-        return(
+    return(   
             <div>
                 <Modal show={shouldShow} onHide={hide}>
-                <Modal.Title> Challenge Inputs</Modal.Title>
-                <Modal.Body>
-                { Object.keys(functions).map((functionName) => (
-                    <Row>
-                    </Row>
-        ))}
-                </Modal.Body>     
+                <Form>
+                    <Modal.Title> Challenge Inputs</Modal.Title>
+                    <Modal.Body>
+                    <FormGroup>
+                        {Object.keys(inputs).map((functionName) => (
+                            <Row>
+                                <Col>{functionName}</Col>
+                                <Col><FormControl type="text" placeholder="Input" onChange = {handleInputChange}/></Col> 
+                                <Col><FormControl type="text" placeholder="Output"/> </Col>
+                            </Row>
+                        ))}
+                    </FormGroup>
+                    </Modal.Body>
+                </Form>   
                 </Modal>
             </div>
         
         );
-    };
-
 };
