c081c35b4700242141bf17ece9b2213e74388e99
Hailey Jang <hailey_jang@berkeley.edu>
2021-01-07 12:27:20 +0900

    Refactored code for websocket interface

--- main/video-feed/main.ts
+++ main/video-feed/main.ts
@@ -5,31 +5,77 @@
 
 //Might need to change to websocket because Janus uses websocket plugin
 
-const ws = new WebSocket('ws://localhost:8080', 'janus-protocol')
+class vidConnection {
+  Socket: WebSocket;
+  sessionID : number
+  pluginID: number
+  janus;
 
-ws.onopen = () => {
-  console.log('Connected to the signaling server')
+  constructor() {
+
+    //WebSocket will be used as signaling channel. 
+    const Socket = new WebSocket('ws://localhost:8080', 'janus-protocol')
+
+    Socket.onopen = () => {
+      console.log('Connected to the signaling channel')
+    }
+
+    Socket.onerror = err => {
+      console.error(err)
+    }
+
+    Socket.onmessage = function(event) {
+      var msg = JSON.parse(event.data);
+      var type = msg.janus;
+    
+      switch(type) {
+        case "error":
+          console.log(msg.error.reason)
+          break;
+        case "success":
+          var trans = msg.transaction;
+          switch(trans) {
+            case "create":
+              var sessionID = msg.data.id
+              break;
+            case "attach":
+              var pluginID = msg.data.id
+          }
+      }
+    }
+  }
+
+  setUpJanus(){
+    this.janus
+    this.Socket.send(JSON.stringify({
+      "janus" : "create",
+      "transaction" : "create"
+    }))
+  }
+
+  setUpPlugin(){
+    this.Socket.send(JSON.stringify({"janus": "attach", "plugin": "janus.plugin.videoroom", "transaction": "attach"}));
+  }
+
+
+
+
+  var peerConnection = new RTCPeerConnection();
+
+  var promise = peerConnection.createAnswer();
+
+  // async function start(){
+  //     try {
+  //         localStream.getTracks().forEach((track: MediaStreamTrack) => {
+  //         peerConnection.addTrack(track, localStream)});
+  //     } catch (err) {
+  //         console.log(err);
+  //     };
+  // }
+
+  // peerConnection.ontrack = (event) => {
+  //     // don't set srcObject again if it is already set.
+  //     if (remoteView.srcObject) return;
+  //     remoteView.srcObject = event.streams[0];
+  // };
 }
-
-ws.onerror = err => {
-  console.error(err)
-}
-
-var peerConnection = new RTCPeerConnection();
-
-var promise = peerConnection.createAnswer();
-
-// async function start(){
-//     try {
-//         localStream.getTracks().forEach((track: MediaStreamTrack) => {
-//         peerConnection.addTrack(track, localStream)});
-//     } catch (err) {
-//         console.log(err);
-//     };
-// }
-
-// peerConnection.ontrack = (event) => {
-//     // don't set srcObject again if it is already set.
-//     if (remoteView.srcObject) return;
-//     remoteView.srcObject = event.streams[0];
-// };
---
+++ main/video-feed/vidServer.ts
@@ -0,0 +1,73 @@
+/* Janus initialization. Taken from Janus documentation.
+*For information on JS-Janus, refer to : https://janus.conf.meetecho.com/docs/JS.html
+*/
+import { Janus } from 'janus-gateway';
+
+
+class vidServer {
+    janus: Janus
+
+    constructor() {
+        Janus.init({
+            debug: true,
+            dependencies: Janus.useDefaultDependencies(), // or: Janus.useOldDependencies() to get the behaviour of previous Janus versions
+            callback: function () {
+                console.log("Server Connected");
+            }
+        });
+
+        //Initializing session
+        this.janus = new Janus(
+            {
+                server: 'http://yourserver:8088/janus',//need to replace with actual host number
+                success: function () {
+                    console.log("Session created");
+                },
+                error: function (cause: string) {
+                    console.log("Error initialization: " + cause);
+                },
+                destroyed: function () {
+                    console.log("Session Destroyed");
+                }
+            });
+    }
+
+    //Initializing video-room plugin
+    plugInInitialize() {
+        this.janus.attach(
+            {
+                plugin: "janus.plugin.videoroom",
+                success: function (pluginHandle) {
+                    // Negotiate WebRTC
+                    var videoRoom = pluginHandle;
+                    var body = { "audio": true, "video": true };
+                    videoRoom.send({ "message": body });
+                    videoRoom.createOffer(
+                        {
+                            media: {
+                                audioSend: true,
+                                videoSend: true,
+                                audioRecv: false,
+                                videoRecv: false,
+                            },
+                            success: function (jsep) {
+                                // Got our SDP! Send our OFFER to the plugin
+                                videoRoom.send({ "message": body, "jsep": jsep });
+                            },
+                            error: function (error) {
+                                console.log(error);
+                            },
+                        });
+                },
+                onlocalstream: function (stream) {
+                    // Invoked after createOffer
+                    // This is our video
+                },
+                /*         onremotestream: function(stream) {
+                                // Invoked after handleRemoteJsep has got us a PeerConnection
+                                // This is the remote video
+                        }, */
+            })
+    }
+}
+
