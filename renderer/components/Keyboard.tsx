0b3ec109992ea73fff46c62049aee13ada1d8d46
Thomas Lee <tcl171@gmail.com>
2021-01-23 14:00:01 -0800

    uncommented handleButtonChange to get rid of angry red lines

--- renderer/components/Keyboard.tsx
+++ renderer/components/Keyboard.tsx
@@ -12,38 +12,33 @@ const keyboardComponent = () => {
         setKey(e.key);
     }
 
-    window.addEventListener("keydown", handleKeyDown);
-
-    /** 
+    window.addEventListener("keydown", handleKeyDown);     
 
     //We will have students define the code for us and put it into an object. How to do that though?
 
-    const buttons = {}; // button will have the format of : button name -> the mapped key
-    const track = [];
-
-    const handleKeyDown = (e: any) => {
-        track[map[buttons[e.key]]] = true;
-    }
-
-    const handleKeyUp = (e: any) => {
-        
-    }
+     // button will have the format of : button name -> the mapped key
+    
+    
+    const buttons = {
+        up:'w',
+        left: 'a',
+        down: 's',
+        right: 'd'
+    };
 
     const handleButtonChange = (button: string) => {
         const handleButtonChangeEvent = (e: React.FormEvent<FormControl & HTMLInputElement>) => {
             buttons[button] = e.currentTarget.value; //change the assigned key to the button
         }
-
         return handleButtonChangeEvent;
     }
 
-    window.addEventListener("keydown", handleKeyDown);
-    window.addEventListener("keyup", handleKeyUp);
-*/
+
     return (
         <Tabs defaultActiveKey={1} animation={false} id="keyboard-tab">
             <Tab eventKey={1} title="Keyboard">
             </Tab>
+
             <Tab eventKey={2} title="Button Config">
                 {Object.keys(map).map(button => (
                     <Row>
