a2d36a770a40b5bb5df50277cd003e81e5655260
Hailey Jang <hailey.jang.01@gmail.com>
2021-03-11 12:39:27 +0900

    Push starter files

---
+++ renderer/components/video-feed/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { VideoFeed } from './VideoFeed';
+
+ReactDOM.render(
+    <VideoFeed />,
+    document.getElementById("Videofeed-react")
+);
