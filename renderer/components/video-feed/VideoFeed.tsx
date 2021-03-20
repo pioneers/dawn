345ce20067098c7f655cbda4a40fe56f695914b4
ewc340 <edwardc1028@berkeley.edu>
2021-03-20 14:12:10 -0700

    Change VideoFeed component to functional

--- renderer/components/video-feed/VideoFeed.tsx
+++ renderer/components/video-feed/VideoFeed.tsx
@@ -1,61 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { keyboardButtons } from '../../consts/keyboard-buttons';
 import { TooltipButton } from '../TooltipButton';
 import { Input, Source } from '../../../protos/protos';
 import { ipcRenderer } from 'electron';
 
-interface State {
-    isKeyboardModeToggled: boolean;
-    keyboardBitmap: number;
-}
+export const VideoFeed = () => {
+  const [isKeyboardModeToggled, setIsKeyboardModeToggled] = useState(false);
+  const [keyboardBitmap, setKeyboardBitmap] = useState(0);
 
-export class VideoFeed extends React.Component<{},State>{
-    
-    constructor(props = {}) {
-        super(props);
-        this.state = {
-          isKeyboardModeToggled: false,
-          keyboardBitmap: 0
-        };
-    }
+  const sendKeyboardInputsToRuntime = () => {
+    const keyboard = new Input({
+      connected: true,
+      axes: [],
+      buttons: keyboardBitmap,
+      source: Source.KEYBOARD
+    });
 
-  sendToRuntime =() => {
-
-      const keyboard = new Input({
-        connected: true,
-        axes: [],
-        buttons: this.state.keyboardBitmap,
-        source: Source.KEYBOARD
-      });
-    
-      ipcRenderer.send('stateUpdate', [keyboard], Source.KEYBOARD);
-  }
-
-  bitShiftLeft = (value: number, numPositions: number) => {
-    return value * Math.pow(2, numPositions);
-  }
-
-
-  // toggle keyboard control and add/remove listening for key presses to control robot
-  toggleKeyboardControl = () => {
-    this.setState({ isKeyboardModeToggled: !this.state.isKeyboardModeToggled });
-
-    if (!this.state.isKeyboardModeToggled) {
-      // We need passive true so that we are able to remove the event listener when we are not in Keyboard Control mode
-      window.addEventListener('keydown', this.turnCharacterOn, { passive: true });
-      window.addEventListener('keyup', this.turnCharacterOff, { passive: true });
-    } else {
-      window.removeEventListener('keydown', this.turnCharacterOn);
-      window.removeEventListener('keyup', this.turnCharacterOff);
-      this.setState({ keyboardBitmap: 0 });
-    }
+    ipcRenderer.send('stateUpdate', [keyboard], Source.KEYBOARD);
   };
 
-  updateKeyboardBitmap = (currentCharacter: string, isKeyPressed: boolean) => {
-    const keyboardNum = keyboardButtons[currentCharacter];
-    let newKeyboardBitmap: number = this.state.keyboardBitmap;
+  const bitShiftLeft = (value: number, numPositions: number) => {
+    return value * Math.pow(2, numPositions);
+  };
 
-    const shift = this.bitShiftLeft(1, keyboardNum);
+  // toggle keyboard control and add/remove listening for key presses to control robot
+  const toggleKeyboardControl = () => {
+    if (!isKeyboardModeToggled) {
+      // We need passive true so that we are able to remove the event listener when we are not in Keyboard Control mode
+      window.addEventListener('keydown', turnCharacterOn, { passive: true });
+      window.addEventListener('keyup', turnCharacterOff, { passive: true });
+    } else {
+      window.removeEventListener('keydown', turnCharacterOn);
+      window.removeEventListener('keyup', turnCharacterOff);
+      setKeyboardBitmap(0);
+    }
+
+    setIsKeyboardModeToggled(!isKeyboardModeToggled);
+  };
+
+  const updateKeyboardBitmap = (currentCharacter: string, isKeyPressed: boolean) => {
+    const keyboardNum = keyboardButtons[currentCharacter];
+    let newKeyboardBitmap: number = keyboardBitmap;
+
+    const shift = bitShiftLeft(1, keyboardNum);
     const MAX_INT32_BITS = 2147483648; // 2^31
 
     const shiftHighBits = shift / MAX_INT32_BITS;
@@ -69,29 +56,27 @@ export class VideoFeed extends React.Component<{},State>{
       newKeyboardBitmap = (shiftHighBits | mapHighBits) * MAX_INT32_BITS + (shiftLowBits | mapLowBits);
     }
 
-    this.setState({ keyboardBitmap: newKeyboardBitmap });
-    this.sendToRuntime();
+    setKeyboardBitmap(newKeyboardBitmap);
+    sendKeyboardInputsToRuntime();
   };
 
-  turnCharacterOff = (e: KeyboardEvent) => {
+  const turnCharacterOff = (e: KeyboardEvent) => {
     // NOT THE ACTION updateKeyboardBitmap. THIS IS A LOCAL FUNCTION
-    this.updateKeyboardBitmap(e.key, false);
-  }
-  turnCharacterOn = (e: KeyboardEvent) => {
-    this.updateKeyboardBitmap(e.key, true)
-  }
+    updateKeyboardBitmap(e.key, false);
+  };
 
-  render(){
-    return (
-        <TooltipButton
-        id="toggleKeyboardControl"
-        text="Toggle Keyboard Control Mode"
-        onClick={this.toggleKeyboardControl}
-        glyph="text-background"
-        disabled={false}
-        bsStyle={this.state.isKeyboardModeToggled ? 'info' : 'default'}
-      />
-    )
-  }
-}
+  const turnCharacterOn = (e: KeyboardEvent) => {
+    updateKeyboardBitmap(e.key, true);
+  };
 
+  return (
+    <TooltipButton
+      id="toggleKeyboardControl"
+      text="Toggle Keyboard Control Mode"
+      onClick={toggleKeyboardControl}
+      glyph="text-background"
+      disabled={false}
+      bsStyle={isKeyboardModeToggled ? 'info' : 'default'}
+    />
+  );
+};
