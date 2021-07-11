f256763b5f4819c2f255af57fb120a9baf8943a7
ewc340 <edwardc1028@berkeley.edu>
2021-07-11 00:47:31 -0700

    Add autocompletion const export

--- renderer/consts/index.ts
+++ renderer/consts/index.ts
@@ -8,6 +8,7 @@ export {
   InfoActionsTypes,
   FieldActionsTypes,
 } from './actions';
+export { AUTOCOMPLETION_LIST } from './autocomplete';
 export { keyboardButtons } from './keyboard-buttons';
 export { ROBOT_STAFF_CODE } from './robotStaffCode';
 export { PeripheralTypes } from './peripheral-types';
