ccbfe5a0833b39204f05cb28ae1e5cb0e361a911
ewc340 <edwardc1028@berkeley.edu>
2021-07-14 20:57:00 -0700

    Add KeyboardButtons type

--- renderer/consts/index.ts
+++ renderer/consts/index.ts
@@ -9,7 +9,7 @@ export {
   FieldActionsTypes,
 } from './actions';
 export { AUTOCOMPLETION_LIST } from './autocomplete';
-export { keyboardButtons } from './keyboard-buttons';
+export { keyboardButtons, KeyboardButtons } from './keyboard-buttons';
 export { ROBOT_STAFF_CODE } from './robotStaffCode';
 export { PeripheralTypes } from './peripheral-types';
 export { VERSION } from './version';
