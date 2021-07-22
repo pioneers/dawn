e01fae7e3a1d0608632c4df06aa50f7c95e419e5
ewc340 <edwardc1028@berkeley.edu>
2021-07-21 20:05:49 -0700

    Lint

--- renderer/components/UpdateBox.tsx
+++ renderer/components/UpdateBox.tsx
@@ -1,9 +1,6 @@
 import { OpenDialogReturnValue, remote } from 'electron';
 import React, { useState } from 'react';
-import {
-  Modal,
-  Button,
-} from 'react-bootstrap';
+import { Modal, Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 import { Client, SFTPWrapper } from 'ssh2';
@@ -30,35 +27,34 @@ interface OwnProps {
 type Props = StateProps & DispatchProps & OwnProps;
 
 export function UpdateBoxContainer(props: Props) {
-  const [isUploading, changeIsUploading] = useState(false)
-  const [updateFilepath, changeUpdateFilepath] = useState('')
+  const [isUploading, changeIsUploading] = useState(false);
+  const [updateFilepath, changeUpdateFilepath] = useState('');
 
   const chooseUpdate = () => {
-    remote.dialog.showOpenDialog({
-      filters: [
-        { name: 'Update Package', extensions: ['zip']}
-      ],
-    }).then((dialogReturn: OpenDialogReturnValue) => {
-      if (dialogReturn.filePaths.length > 0) {
-        changeUpdateFilepath(dialogReturn.filePaths[0]);
-      }
-    });
-  }
+    remote.dialog
+      .showOpenDialog({
+        filters: [{ name: 'Update Package', extensions: ['zip'] }]
+      })
+      .then((dialogReturn: OpenDialogReturnValue) => {
+        if (dialogReturn.filePaths.length > 0) {
+          changeUpdateFilepath(dialogReturn.filePaths[0]);
+        }
+      });
+  };
 
   const upgradeSoftware = () => {
-    changeIsUploading(true)
+    changeIsUploading(true);
     const RUNTIME_ZIP_REMOTE_PATH = '/tmp/runtime.zip';
 
     const conn = new Client();
-    conn.on('ready', () => {
-      conn.sftp((err: Error | undefined, sftp: SFTPWrapper) => {
-        if (err) {
-          logging.log(err.message);
-        } else {
-          logging.log('SSH Connection');
-          sftp.fastPut(
-            updateFilepath,
-            RUNTIME_ZIP_REMOTE_PATH, (err2: any) => {
+    conn
+      .on('ready', () => {
+        conn.sftp((err: Error | undefined, sftp: SFTPWrapper) => {
+          if (err) {
+            logging.log(err.message);
+          } else {
+            logging.log('SSH Connection');
+            sftp.fastPut(updateFilepath, RUNTIME_ZIP_REMOTE_PATH, (err2: any) => {
               conn.end();
               changeIsUploading(false);
               props.hide();
@@ -66,82 +62,76 @@ export function UpdateBoxContainer(props: Props) {
                 props.onAlertAdd(
                   'Robot Connectivity Error',
                   `Dawn was unable to upload the update to the robot.
-                  Please check your connectivity, or try restarting the robot.`,
+                  Please check your connectivity, or try restarting the robot.`
                 );
                 logging.log(err2);
               } else {
                 props.onAlertAdd(
                   'Robot Update Initiated',
                   `Update is installing and Runtime will restart soon.
-                  Please leave your robot on for the next 1 minute.`,
+                  Please leave your robot on for the next 1 minute.`
                 );
               }
-            },
-          );
-        }
+            });
+          }
+        });
+      })
+      .connect({
+        debug: (debugInfo: string) => {
+          logging.log(debugInfo);
+        },
+        host: props.ipAddress,
+        port: defaults.PORT,
+        username: defaults.USERNAME,
+        password: defaults.PASSWORD
       });
-    }).connect({
-      debug: (debugInfo: string) => { logging.log(debugInfo); },
-      host: props.ipAddress,
-      port: defaults.PORT,
-      username: defaults.USERNAME,
-      password: defaults.PASSWORD,
-    });
-  }
+  };
 
   const disableUploadUpdate = () => {
-    return (
-      !updateFilepath ||
-      isUploading ||
-      !(props.connectionStatus && props.runtimeStatus) ||
-      props.isRunningCode
+    return !updateFilepath || isUploading || !(props.connectionStatus && props.runtimeStatus) || props.isRunningCode;
+  };
+
+  const { shouldShow, hide } = props;
+  let modalBody = null;
+
+  if (isUploading) {
+    modalBody = (
+      <Modal.Body>
+        <h4>PLEASE DO NOT TURN OFF ROBOT</h4>
+        <br />
+      </Modal.Body>
+    );
+  } else {
+    modalBody = (
+      <Modal.Body>
+        <h4>Update Package (.zip file)</h4>
+        <h5>{updateFilepath ? updateFilepath : ''}</h5>
+        <Button type="button" onClick={chooseUpdate}>
+          Choose File
+        </Button>
+        <br />
+      </Modal.Body>
     );
   }
-
-    const { shouldShow, hide } = props;
-    let modalBody = null;
-
-    if (isUploading) {
-      modalBody = (
-        <Modal.Body>
-          <h4>PLEASE DO NOT TURN OFF ROBOT</h4>
-          <br />
-        </Modal.Body>
-      );
-    } else {
-      modalBody = (
-        <Modal.Body>
-          <h4>Update Package (.zip file)</h4>
-          <h5>{updateFilepath ? updateFilepath : ''}</h5>
-          <Button type="button" onClick={chooseUpdate}>Choose File</Button>
-          <br />
-        </Modal.Body>
-      );
-    }
-    return (
-      <Modal show={shouldShow} onHide={hide} animation={false}>
-        <Modal.Header closeButton>
-          <Modal.Title>Upload Update</Modal.Title>
-        </Modal.Header>
-        {modalBody}
-        <Modal.Footer>
-          <Button
-            type="button"
-            variant="primary"
-            onClick={upgradeSoftware}
-            disabled={disableUploadUpdate()}
-          >
-            {isUploading ? 'Uploading...' : 'Upload Files'}
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    );
+  return (
+    <Modal show={shouldShow} onHide={hide} animation={false}>
+      <Modal.Header closeButton>
+        <Modal.Title>Upload Update</Modal.Title>
+      </Modal.Header>
+      {modalBody}
+      <Modal.Footer>
+        <Button type="button" variant="primary" onClick={upgradeSoftware} disabled={disableUploadUpdate()}>
+          {isUploading ? 'Uploading...' : 'Upload Files'}
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
   onAlertAdd: (heading: string, message: string) => {
     dispatch(addAsyncAlert(heading, message));
-  },
+  }
 });
 
 export const UpdateBox = connect(null, mapDispatchToProps)(UpdateBoxContainer);
