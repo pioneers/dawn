1ae913cab96eccc117cab69cc7c757394b276323
Edward Choi <edwardc1028@berkeley.edu>
2022-03-16 19:44:38 -0700

    Update release script to provide better zip compression (#154)
    * Add back async alerts
    
    * Update release script with better zip compression
    
    * Add concurrency limit when zipping
    
    * Add guard to return early if shouldn't zip
    
    * Filter booleans out of appPaths when zipping
    
    * Revert "Add back async alerts"
    
    This reverts commit 3201204313dbf15e3b2f95c4df9a2c877b887917.

--- release.ts
+++ release.ts
@@ -1,14 +1,40 @@
 /* eslint-disable */
 
+import archiver from 'archiver';
+import { map } from 'bluebird';
+import fs from 'fs';
 import minimist from 'minimist';
-import packager, { Options } from 'electron-packager';
+import packager, { Options, PlatformOption, TargetArch } from 'electron-packager';
 import path from 'path';
-import { spawn } from 'child_process';
+
+interface Args extends minimist.ParsedArgs {
+  arch?: TargetArch;
+  platform?: PlatformOption;
+  shouldZip?: boolean;
+}
+
+async function zip(srcPath: string) {
+  const theZipper = archiver('zip', { zlib: { level: 9 } }); // level 9 uses max compression
+  const outputZipFilePath = `${srcPath}.zip`;
+  const output = fs.createWriteStream(outputZipFilePath);
+
+  theZipper
+    .directory(srcPath, false)
+    .pipe(output)
+    .on('finish', () => {
+      console.log(`Finished zipping ${outputZipFilePath}`);
+    })
+    .on('error', (err) => {
+      console.error(`Failed to zip file with source path ${srcPath} -- err: ${err}`);
+    });
+
+  await theZipper.finalize();
+}
 
 /* General release command: 'ts-node release.ts'
- * For a specific target: 'ts-node release.ts --platform=... --arch=...'
+ * For a specific target: 'ts-node release.ts --platform=... --arch=... --shouldZip=...'
  */
-async function pack(args: minimist.ParsedArgs) {
+async function pack(args: Args) {
   const packageOptions: Options = {
     dir: __dirname, // source dir
     name: 'dawn',
@@ -22,19 +48,30 @@ async function pack(args: minimist.ParsedArgs) {
   });
 
   // platform is either 'darwin', 'linux', or 'win32'
-  packageOptions.platform = args.platform ? args.platform : 'all';
+  packageOptions.platform = args.platform ?? 'all';
   console.log('Packaging for: ', packageOptions.platform);
 
-  const appPaths: (string | boolean)[] = await packager(packageOptions);
+  const appPaths: Array<string | boolean> = await packager(packageOptions);
 
-  appPaths.map((appPath: string | boolean) => {
-    if (appPath == true) {
-      return;
-    }
-    console.log(`Zipping ${appPath}`);
+  // Should zip the packages for each platform by default
+  const shouldZip = args.shouldZip ?? true;
 
-    spawn('zip', ['-r', `${appPath}.zip`, `${appPath}`], { stdio: 'inherit' });
-  });
+  if (!shouldZip) {
+    return;
+  }
+
+  // If appPath is a boolean, then we assume package for platform already exists, so no need to do anything
+  const appPathsToZip = appPaths.filter((appPath) => typeof appPath == 'string') as string[];
+
+  map(
+    appPathsToZip,
+    async (appPath: string) => {
+      console.log(`Zipping ${appPath}`);
+
+      await zip(appPath);
+    },
+    { concurrency: appPaths.length }
+  );
 }
 
 pack(minimist(process.argv.slice(2))).then(() => console.log('Packaging Done'));
