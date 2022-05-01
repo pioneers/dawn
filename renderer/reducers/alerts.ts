f429e5fa36bb3e3d5966ab7e00e00a2ff6a3c87d
Meshan Khosla <mkhosla@berkeley.edu>
2022-05-01 13:12:52 -0700

    Made changes

--- renderer/reducers/alerts.ts
+++ renderer/reducers/alerts.ts
@@ -7,11 +7,13 @@ import { AddAsyncAlertAction, RemoveAsyncAlertAction } from '../types';
 
 type Actions = AddAsyncAlertAction | RemoveAsyncAlertAction;
 
-type AsyncAlertsState = Array<{
-  id?: number;
+interface Alert {
+  id: number;
   heading?: string;
   message?: string;
-}>;
+}
+
+type AsyncAlertsState = Array<Alert>;
 
 const initialState: AsyncAlertsState = [];
 
@@ -23,8 +25,8 @@ export const asyncAlerts = (state: AsyncAlertsState = initialState, action: Acti
         {
           id: action.id,
           heading: action.heading,
-          message: action.message,
-        },
+          message: action.message
+        }
       ];
     case consts.AlertActionsTypes.REMOVE_ASYNC_ALERT:
       return state.filter((el: { id?: number }) => el.id !== action.id);
