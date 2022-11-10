9ae73257c764619dba4a624d99f540f6abc8dca4
Hailey Jang <55821834+HaileyJang@users.noreply.github.com>
2022-11-09 20:11:06 -0800

    Ease Race Condition (#168)

--- main/networking/Runtime.ts
+++ main/networking/Runtime.ts
@@ -165,10 +165,11 @@ class RuntimeConnection {
           this.socket.connect(port, ip);
         }
       }
-    }, 1000);
+    }, 3000);
 
     this.socket.on('connect', () => {
       this.logger.log('Runtime connected');
+      
       this.socket.write(new Uint8Array([1])); // Runtime needs first byte to be 1 to recognize client as Dawn (instead of Shepherd)
     });
 
